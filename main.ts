3d85a81b09e80ac066d7b9d1423d1af3d682e5b5
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-19 13:37:52 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-function nastav_diodu (param: number) {
+function nastav_diodu(param: number) {
     pins.digitalWritePin(DigitalPin.P0, param)
 }
-function zhasni_diodu2 () {
+
+function zhasni_diodu2() {
     pins.digitalWritePin(DigitalPin.P0, 0)
 }
-function rozsvit_diodu () {
+
+function rozsvit_diodu() {
     pins.digitalWritePin(DigitalPin.P0, 1)
 }
-basic.forever(function () {
+
+basic.forever(function on_forever() {
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P0, 0)
