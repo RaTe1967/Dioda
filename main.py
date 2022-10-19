c52b871b747039d9d22702209d46254b41ae1f3a
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-19 13:36:55 +0200

    Aktualizovat main.blocks, main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def nastav_diodu(číslo: number):
-    pass
+def nastav_diodu(param: number):
+    pins.digital_write_pin(DigitalPin.P0, param)
 def zhasni_diodu2():
     pins.digital_write_pin(DigitalPin.P0, 0)
 def rozsvit_diodu():
     pins.digital_write_pin(DigitalPin.P0, 1)
 
 def on_forever():
-    pass
+    pins.digital_write_pin(DigitalPin.P0, 1)
+    basic.pause(500)
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    basic.pause(200)
 basic.forever(on_forever)
