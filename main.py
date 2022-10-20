96c4d451fac4373a3591bf9cad4e3f4dc41473dd
RaTe1967 <115639425+RaTe1967@users.noreply.github.com>
2022-10-20 13:37:12 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,71 @@
 def zhasni_diodu():
     pins.digital_write_pin(DigitalPin.P0, 0)
+def blikej(barva: number):
+    while True:
+        if barva == RED:
+            pins.digital_write_pin(DigitalPin.P0, 1)
+            basic.pause(500)
+            pins.digital_write_pin(DigitalPin.P0, 0)
+            basic.pause(200)
+        else:
+            if barva == GREEN:
+                pins.digital_write_pin(DigitalPin.P1, 1)
+                basic.pause(500)
+                pins.digital_write_pin(DigitalPin.P1, 0)
+                basic.pause(200)
+            else:
+                pins.digital_write_pin(DigitalPin.P2, 1)
+                basic.pause(500)
+                pins.digital_write_pin(DigitalPin.P2, 0)
+                basic.pause(200)
+
+def on_button_pressed_a():
+    blikej(GREEN)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 def nastav_diodu(param: number):
     pins.digital_write_pin(DigitalPin.P0, param)
+
+def on_button_pressed_b():
+    prepni_diodu(BLUE)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def prepni_diodu(barva2: number):
+    global red_is_on, green_is_on, blue_is_on
+    if barva2 == RED:
+        if red_is_on == 1:
+            red_is_on = 0
+        else:
+            red_is_on = 1
+        pins.digital_write_pin(DigitalPin.P0, blue_is_on)
+    else:
+        if barva2 == GREEN:
+            if green_is_on == 1:
+                green_is_on = 0
+            else:
+                green_is_on = 1
+            pins.digital_write_pin(DigitalPin.P1, green_is_on)
+        else:
+            if barva2 == BLUE:
+                if blue_is_on == 1:
+                    blue_is_on = 0
+                else:
+                    blue_is_on = 1
+                pins.digital_write_pin(DigitalPin.P2, blue_is_on)
 def rozsvit_diodu():
     pins.digital_write_pin(DigitalPin.P0, 1)
+green_is_on = 0
+blue_is_on = 0
+red_is_on = 0
+BLUE = 0
+GREEN = 0
+RED = 0
+RED = 0
+GREEN = 1
+BLUE = 2
+red_is_on = 0
+blue_is_on = 0
+green_is_on = 0
 
 def on_forever():
     pins.digital_write_pin(DigitalPin.P0, 1)
--- main.ts
+++ main.ts
@@ -1,12 +1,80 @@
 function zhasni_diodu () {
     pins.digitalWritePin(DigitalPin.P0, 0)
 }
+function blikej (barva: number) {
+    while (true) {
+        if (barva == RED) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            basic.pause(200)
+        } else {
+            if (barva == GREEN) {
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                basic.pause(200)
+            } else {
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                basic.pause(200)
+            }
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    blikej(GREEN)
+})
 function nastav_diodu (param: number) {
     pins.digitalWritePin(DigitalPin.P0, param)
+}
+input.onButtonPressed(Button.B, function () {
+    prepni_diodu(BLUE)
+})
+function prepni_diodu (barva: number) {
+    if (barva == RED) {
+        if (red_is_on == 1) {
+            red_is_on = 0
+        } else {
+            red_is_on = 1
+        }
+        pins.digitalWritePin(DigitalPin.P0, blue_is_on)
+    } else {
+        if (barva == GREEN) {
+            if (green_is_on == 1) {
+                green_is_on = 0
+            } else {
+                green_is_on = 1
+            }
+            pins.digitalWritePin(DigitalPin.P1, green_is_on)
+        } else {
+            if (barva == BLUE) {
+                if (blue_is_on == 1) {
+                    blue_is_on = 0
+                } else {
+                    blue_is_on = 1
+                }
+                pins.digitalWritePin(DigitalPin.P2, blue_is_on)
+            }
+        }
+    }
 }
 function rozsvit_diodu () {
     pins.digitalWritePin(DigitalPin.P0, 1)
 }
+let green_is_on = 0
+let blue_is_on = 0
+let red_is_on = 0
+let BLUE = 0
+let GREEN = 0
+let RED = 0
+RED = 0
+GREEN = 1
+BLUE = 2
+red_is_on = 0
+blue_is_on = 0
+green_is_on = 0
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(500)
